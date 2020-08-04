37d7c7c8680060769b4806b45159312d2f9f1074
Jalaj <Jalaj@mail.com>
2020-08-04 12:02:42 +0530

    setup react router

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import * as serviceWorker from './serviceWorker';
+import './styles/index.css';
+import { Home, User, Listings, Listing, Host, NotFound } from './sections';
 
-ReactDOM.render(<React.StrictMode>Hello World!</React.StrictMode>, document.getElementById('root'));
+const client = new ApolloClient({
+	uri: 'http://localhost:9000/api',
+	cache: new InMemoryCache(),
+});
+
+const App = () => (
+	<Router>
+		<Switch>
+			<Route exact path="/" component={Home} />
+			<Route exact path="/host" component={Host} />
+			<Route exact path="/listing/:id" component={Listing} />
+			<Route exact path="/listings/:location?" component={Listings} />
+			<Route exact path="/user/:id" component={User} />
+			<Route component={NotFound} />
+		</Switch>
+	</Router>
+);
+
+ReactDOM.render(
+	<ApolloProvider client={client}>
+		<App />
+	</ApolloProvider>,
+	document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
