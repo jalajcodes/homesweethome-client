65b21a7ff412ea277f15a64831d554aedf01267e
codewithjalaj <jalaj@mail.com>
2020-08-26 15:00:32 +0530

    completed most of the homepage

--- src/sections/Home/components/HomeHeroArea/index.tsx
+++ src/sections/Home/components/HomeHeroArea/index.tsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import { Card, Col, Input, Row, Typography } from 'antd';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 import torontoImage from '../../assets/toronto.jpg';
 import dubaiImage from '../../assets/dubai.jpg';
 import losAngelesImage from '../../assets/los-angeles.jpg';
 import londonImage from '../../assets/london.jpg';
+import { displayErrorMessage } from '../../../../lib/utils';
 
 const { Title } = Typography;
 const { Search } = Input;
 
 export const HomeHeroArea = () => {
+	const history = useHistory();
+
+	const onSearch = (value: string) => {
+		const trimmedValue = value.trim();
+		if (trimmedValue.length === 0) return displayErrorMessage(`Please enter a valid search!`);
+		history.push(`/listings/${trimmedValue}`);
+	};
+
 	return (
 		<div className="home-hero">
 			<div className="home-hero__search">
 				<Title className="home-hero__title">Find a place you'll love to stay at...</Title>
 				<Search
+					onSearch={onSearch}
 					placeholder="Search 'San Francisco'"
 					size="large"
 					enterButton
